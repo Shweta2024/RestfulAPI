d7ebb44eb8e7c1813f2ce95c15da7a9e85fb39ac
Shweta Bhagat <bhagatshweta0216@gmail.com>
2023-10-14 18:47:50 +0530

    connect to db

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+require("dotenv").config()
 const express = require("express")
+const mongoose = require("mongoose")
 const PORT = process.env.PORT || 5000
 const app = express()
 
+const db_url =  "mongodb+srv://admin-shweta:"
+            +   `${process.env.DB_PASSWORD}`
+            +   "@cluster0.vtowurn.mongodb.net/RestDB";
+            
+mongoose.connect(db_url)
 
 app.listen(PORT, (req, res) => {
     console.log(`server started at port : ${PORT}`)
