c5df0e302c0c14269903e5b25adf30594ff517a7
Shweta Bhagat <bhagatshweta0216@gmail.com>
2023-10-14 18:50:22 +0530

    add posts route

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const mongoose = require("mongoose")
 const PORT = process.env.PORT || 5000
 const app = express()
 
+//  import route
+const postsRoute = require("./routes/posts")
+app.use("/posts", postsRoute)
+
 const db_url =  "mongodb+srv://admin-shweta:"
             +   `${process.env.DB_PASSWORD}`
             +   "@cluster0.vtowurn.mongodb.net/RestDB";
-            
+
+//  connect to db
 mongoose.connect(db_url)
 
 app.listen(PORT, (req, res) => {
